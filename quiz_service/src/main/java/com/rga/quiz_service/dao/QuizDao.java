63f779d6581d5caa3383625f1afa4ff866e93e07
RodrigoAGA <rodrigogalvez2706@gmail.com>
2025-09-16 17:00:16 -0500

    question service + quiz service

---
+++ quiz_service/src/main/java/com/rga/quiz_service/dao/QuizDao.java
@@ -0,0 +1,7 @@
+package com.rga.quiz_service.dao;
+
+import com.rga.quiz_service.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizDao extends JpaRepository<Quiz,Integer> {
+}
