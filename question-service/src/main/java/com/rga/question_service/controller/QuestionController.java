937beb438c922e48c1a2c69c508171fb143bda7f
RodrigoAGA <rodrigogalvez2706@gmail.com>
2025-09-17 13:24:19 -0500

    service registry + api gateway

--- question-service/src/main/java/com/rga/question_service/controller/QuestionController.java
+++ question-service/src/main/java/com/rga/question_service/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package com.rga.question_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,9 @@ public class QuestionController {
 
     @Autowired
     QuestionService questionService;
+
+    @Autowired
+    Environment environment;
 
     @GetMapping("allQuestions")
     public ResponseEntity<List<Question>> getAllQuestions(){
@@ -40,6 +44,7 @@ public class QuestionController {
 
     @PostMapping("getQuestions")
     public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds){
+        // System.out.println(environment.getProperty("local.server.port"));
         return questionService.getQuestionsFromId(questionIds);
     }
 
---
+++ quiz-service/src/main/java/com/rga/quiz_service/service/QuizService.java
@@ -0,0 +1,51 @@
+package com.rga.quiz_service.service;
+
+import com.rga.quiz_service.dao.QuizDao;
+import com.rga.quiz_service.feign.QuizInterface;
+import com.rga.quiz_service.model.QuestionWrapper;
+import com.rga.quiz_service.model.Quiz;
+import com.rga.quiz_service.model.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuizService {
+
+    @Autowired
+    QuizDao quizDao;
+    
+    @Autowired
+    QuizInterface quizInterface;
+
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+        List<Integer> questions= quizInterface.getQuestionsForQuiz(category, numQ).getBody();
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestionsIds(questions);
+        quizDao.save(quiz);
+
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
+
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Integer> questionIds = quiz.getQuestionsIds();
+        quizInterface.getQuestionsFromId(questionIds);
+        ResponseEntity<List<QuestionWrapper>> questions = quizInterface.getQuestionsFromId(questionIds);
+        return questions;
+
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        
+        ResponseEntity<Integer> score = quizInterface.getScore(responses);
+
+        return score;
+    }
+}
